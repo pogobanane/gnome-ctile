f3516d39913db11146520b2d47edde4e0b9d9c31
Pogobanane <aenderboy@gmx.de>
2022-06-16 10:55:44 +0200

    make ctile default package

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-linux.ctile = nixpkgs.legacyPackages.x86_64-linux.callPackage ./ctile.nix {};
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ctile;
 
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = with pkgs; [
